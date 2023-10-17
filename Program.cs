a8a471975da51e59f03b369c0e508bf48b0356d4
ChasAlex <145579430+ChasAlex@users.noreply.github.com>
2023-10-17 15:21:09 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Sjukhuset
+namespace Sjukhuset
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Doctor myDoctor = new Doctor(100000, "Tom");
+            Pasient myPatient = new Pasient("Hjalmar");
+            Nurse myNurse = new Nurse("Charlotte", true);
+
+            Console.WriteLine();
+
+            
+        
+        
+        
         }
     }
 
